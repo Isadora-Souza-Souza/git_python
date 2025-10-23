f2c9020b520d58e40f38489d3e94a75ba192ed5c
Isadora Souza <isadoralusouza@gmail.com>
2025-10-22 21:15:14 -0300

    Adiciona funcao de subtracao

--- calculadora.py
+++ calculadora.py
@@ -1,9 +1,18 @@
 soma = 0
 resposta = 'S'  
 
+
 while resposta == 'S' or resposta == 's':
     num = float(input("Digite um número: "))
     soma = soma + num
     resposta = input("Deseja continuar? (S/N) ")
 
 print(f"Soma dos números: {soma}")
+
+
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+resposta = input("Deseja continuar? (S/N) ")
+subtracao = num1 - num2
+
+print(f"Subtração dos números: {subtracao}")
